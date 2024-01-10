dbd18effa2cadc03cf914923008aa4aee399dff2
1000thing2 <55175977+kochunmyung@users.noreply.github.com>
2024-01-10 17:55:57 +0900

    Time: 868 ms (72.97%), Space: 65.5 MB (46.05%) - LeetHub

---
+++ 0077-combinations/0077-combinations.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def combine(self, n, k):
+        results = []
+        
+        def dfs(elements, start, k):
+            if k == 0:
+                results.append(elements[:])
+                return
+            
+            for i in range(start, n+1):
+                elements.append(i)
+                dfs(elements, i+1, k-1)
+                elements.pop()
+                
+        
+        dfs([],1,k)
+        return results
+        
